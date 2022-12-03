00818c09e65f09116ca5b6192b6233dcbe426c01
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-12-03 12:25:08 -0600

    Solve day 3 part 2
    Adds solution to problem 2. Prints output.
    Refactored code to fit both solutions.
    Adds description.

--- day3/challenge.py
+++ day3/challenge.py
@@ -1,14 +1,34 @@
 with open('day3/rucksacks.txt', 'r') as sacks:
-  total = 0
-  for sack in sacks.readlines():
-    sack = sack.strip()
-    size = len(sack)
-    cmprta = sack[0:size//2]
-    cmprtA = sack[size//2:]
-    char = [i for i in cmprta if i in cmprtA][0]
-    total += ord(char.swapcase()) - 64 - 6*(char.isupper())
+  repeatedTotal = 0
+  badgesTotal = 0
 
+  def read_lines_w_step(step, lines):
+    cache = [line.strip() for line in lines.readlines()]
+    cnt = -step
+    for _ in range(len(cache)//step):
+      cnt += step
+      yield cache[cnt:cnt+step]
+  
+  intersection = lambda A, B: [i for i in A if i in B]
+  calc_value = lambda char: ord(char.swapcase()) - 64 - 6*(char.isupper())
 
-  print (total)
+  def read3lines(sacks):
+    total = 0
+    for sack in sacks:
+      sack = sack.strip()
+      size = len(sack)
+      cmprta = sack[0:size//2]
+      cmprtA = sack[size//2:]
+      char = intersection(cmprta, cmprtA)[0]
+      total += calc_value(char)
+    return total
+  
+  for sackGroup in read_lines_w_step(3, sacks):
+    repeatedTotal += read3lines(sackGroup)
+    a, b, c = sackGroup
+    badgesTotal += calc_value(intersection(intersection(a, b), c)[0])
+
+  print (repeatedTotal)
+  print (badgesTotal)
 
 sacks.close()
